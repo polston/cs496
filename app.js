5de1d400e9edb1d1e4c57c6cae72f4cd5e11d8e9
polston <polston@gmail.com>
2017-01-30 12:24:09 -0600

    Create skeleton for node project.

---
+++ app.js
@@ -0,0 +1,21 @@
+let express = require('express');
+
+let indexController = require('./controllers/indexController');
+let errorController = require('./controllers/errorController');
+
+let app = express();
+
+//set up template engine
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+//fire controllers
+indexController(app);
+errorController(app);
+
+//listen to port
+app.listen(3000);
+console.log('listening to port 3000');
---
+++ controllers/indexController.js
@@ -0,0 +1,20 @@
+module.exports = function(app){
+
+  //pretend this was pulled from a database
+  let people = [{name: 'Michael'},
+              {name: 'David'},
+              {name: 'Nathan'},
+              {name: 'Austin'}];
+
+  //definitely a better way to do '/' and '/index'
+  app.get('/', function(req, res){
+    console.log(req.url);
+    res.render('index', {group: people});
+  });
+  
+  app.get('/index', function(req, res){
+    console.log(req.url);
+    res.render('index', {group: people});
+  });
+
+};
