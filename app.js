dc6d8bac55a563e151be9376f05ade16f1f59e50
Michael Polston <polston@gmail.com>
2017-04-15 22:18:03 -0500

    Resolve merge artifacts

--- app.js
+++ app.js
@@ -10,29 +10,8 @@ let dbconfig = require('./dbconfig')
 
 mongoose.Promise = global.Promise
 
-<<<<<<< HEAD
 //depends on process.env.TEST, either 'true' or 'false'
 mongoose.connect(dbconfig.db)
-
-//includes the routes, probably a way to just batch include all of them
-//but, I don't know how (yet)
-let indexRoutes = require('./routes/indexRoutes');
-let whateverRoutes = require('./routes/whateverRoutes');
-let errorRoutes = require('./routes/errorRoutes');
-let controllerRoutes = require('./routes/controllerRoutes')
-let calendarRoutes = require('./routes/calendarRoutes')
-
-//creates an express application
-//mostly magic
-let app = express()
-
-//static & public files
-app.use(express.static(__dirname + '/public'))
-
-//used to parse the 'body' of http requests into json
-app.use(bodyParseer.json())
-=======
-mongoose.connect('mongodb://localhost/cs496')
 //mongoose.createConnection('mongodb://localhost/cs496')
 
 //creates an express application
@@ -41,11 +20,11 @@ let app = express();
 
 //includes the routes, probably a way to just batch include all of them
 //but, I don't know how (yet)
-let indexRoutes = require('./routes/index');
-let whateverRoutes = require('./routes/whatever');
-let errorRoutes = require('./routes/error');
-let controllerRoutes = require('./routes/controllers');
-let loginRoutes = require('./routes/login');
+let indexRoutes = require('./routes/indexRoutes');
+let whateverRoutes = require('./routes/whateverRoutes');
+let errorRoutes = require('./routes/errorRoutes');
+let controllerRoutes = require('./routes/controllerRoutes')
+let calendarRoutes = require('./routes/calendarRoutes')
 
 
 
@@ -61,7 +40,6 @@ app.use(passport.initialize());                                        //whateve
 app.use(passport.session());                                            //sec...well its redirecting now lol
 require('./config/passport')(passport);
 app.use(bodyParseer.json())                                             
->>>>>>> 9936d07... Added Google OAuth2 Sign-In
 
 //set up template engine
 app.set('view engine', 'ejs');
@@ -75,19 +53,12 @@ app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/c
 app.use('/angular', express.static(__dirname + '/node_modules/angular/'))
 
 //fire routes
-<<<<<<< HEAD
-controllerRoutes(app)
-indexRoutes(app)
-calendarRoutes(app)
-whateverRoutes(app)
-errorRoutes(app)
-=======
 controllerRoutes(app);
 loginRoutes(app,passport);
 indexRoutes(app);
+calendarRoutes(app)
 whateverRoutes(app);
 errorRoutes(app);
->>>>>>> 9936d07... Added Google OAuth2 Sign-In
 
 
 //listen to port 4000
