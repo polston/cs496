c599926770124a76d218224aff3229d4645f9a1d
Michael Polston <polston@gmail.com>
2017-04-15 23:04:57 -0500

    Get sign in to work locally, add secrets file for google oauth

--- models/userModel.js
+++ models/userModel.js
@@ -12,41 +12,41 @@ const UserSchema = new Schema({
     name: String
   },
   name: {
-//     firstName: {
-//       type: String,
-//       minlength: [1, 'Last name must contain at least one character'],
-//       maxlength: [25, 'Last name is too long.'],
-//       required: [true, 'First name requried.'],
-//     },
-//     lastName: {
-//       type: String,
-//       minlength: [1, 'First name must contain at least one character'],
-//       maxlength: [25, 'First name is too long.'],
-//       required: [true, 'Last name required.'],
-//   }
-// },
-//   courses: [String],
-//   permissions: {
-//     type: String, 
-//     default: 'Student', 
-//     trim: true, 
-//     required: true,
-//     validate: {
-//       validator: function(v) { 
-//         return validPermissions.indexOf(v) != -1 
-//       }, 
-//       message: "Invalid permissions"
-//     },
-//   }
+    firstName: {
+      type: String,
+      minlength: [1, 'Last name must contain at least one character'],
+      maxlength: [25, 'Last name is too long.'],
+      // required: [true, 'First name requried.'],
+    },
+    lastName: {
+      type: String,
+      minlength: [1, 'First name must contain at least one character'],
+      maxlength: [25, 'First name is too long.']
+      // required: [true, 'Last name required.'],
+  }
+},
+  courses: [String],
+  permissions: {
+    type: String, 
+    default: 'Student', 
+    trim: true, 
+    required: true,
+    validate: {
+      validator: function(v) { 
+        return validPermissions.indexOf(v) != -1 
+      }, 
+      message: "Invalid permissions"
+    },
+  }
 
     //austin's branch is here, validators will be missing
     //until we resolve how to use them with the oauth routing
-    firstName: String,// firstName: {type: String, required: true},
-    lastName: String// lastName: {type: String, required: true}
-  },
-  courses: [String],
-  permissions: {type: String, default: 'Student', required: true},
-  email: String// email: {type: String, required: true}
+  //   firstName: String,// firstName: {type: String, required: true},
+  //   lastName: String// lastName: {type: String, required: true}
+  // },
+  // courses: [String],
+  // permissions: {type: String, default: 'Student', required: true},
+  // email: String// email: {type: String, required: true}
 })
 
 const User = mongoose.model('user', UserSchema)
